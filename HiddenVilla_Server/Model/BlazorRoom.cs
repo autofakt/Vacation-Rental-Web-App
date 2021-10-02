e4f5963e84e4f595ee115b71716ce766d12e9b6a
Mike Be <autofakt5@gmail.com>
2021-10-02 14:29:48 -0700

    Add project files.

---
+++ HiddenVilla_Server/Model/BlazorRoom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Model
+{
+    public class BlazorRoom
+    {
+        public int ID { get; set; }
+        public string RoomName{ get; set; }
+        public double price { get; set; }
+        public bool IsActive { get; set; }
+
+        public List<BlazorRoomProp> RoomProps { get; set; }
+    }
+}
