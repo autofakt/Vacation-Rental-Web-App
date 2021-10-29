911d6ad0023ee2bf4c9b0d09e17b6719569b72d5
Mike Be <autofakt5@gmail.com>
2021-10-29 11:49:34 -0700

    Added hotelRoomImages Model, 80 done.

---
+++ Business/Repository/IRepository/IHotelImageRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace Business.Repository.IRepository
+{
+    interface IHotelImageRepository
+    {
+        public Task<int> CreateHotelRoomImage(HotelRoomImageDTO image);
+        public Task<int> DeleteHotelRoomImageByImageId(int imageId);
+        public Task<int> DeleteHotelRoomImageByRoomId(int roomId);
+
+        public Task<IEnumerable<HotelRoomImageDTO>> GetHotelRoomImages(int roomId);
+    }
+}
