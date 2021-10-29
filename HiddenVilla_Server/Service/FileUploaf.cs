85912220a6e17d1370030044887681fb02ee8cfe
Mike Be <autofakt5@gmail.com>
2021-10-29 13:41:35 -0700

    Ifile interface implementation, 82 done

---
+++ HiddenVilla_Server/Service/FileUploaf.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Service
+{
+    public class FileUploaf : IFileUpload
+    {
+        public bool DeleteFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UploadFile(IBrowserFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
