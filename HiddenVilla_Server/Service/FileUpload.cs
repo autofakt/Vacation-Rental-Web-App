c7b83b8ca5eaf8f126fc9bd71d7df490252786ad
Mike Be <autofakt5@gmail.com>
2021-10-29 17:49:18 -0700

    file reading for images, 83 done.

---
+++ HiddenVilla_Server/Service/FileUpload.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Service
+{
+    public class FileUpload : IFileUpload
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public bool DeleteFile(string fileName)
+        {
+            bool status = false;
+            try
+            {
+                var path = $"{_webHostEnvironment.WebRootPath}\\RoomImages\\{fileName}";
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                    return true;
+                }
+                return false;
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<string> UploadFile(IBrowserFile file)
+        {
+            try //dont understand this code well
+            {
+                FileInfo fileInfo = new FileInfo(file.Name);
+                var fileName = Guid.NewGuid().ToString() + fileInfo.Extension;
+                var folderDirectory = $"(_webHostEnvironment.WebRootPath}\\RoomImages";
+                var path = Path.Combine(_webHostEnvironment.WebRootPath, "RoomImages", fileName);
+
+                var memoryStream = new MemoryStream();
+                await file.OpenReadStream().CopyToAsync(memoryStream);
+
+                if (!Directory.Exists(folderDirectory))
+                {
+                    Directory.CreateDirectory(folderDirectory);
+                }
+
+                await using (var fs= new FileStream(path,FileMode.Create, FileAccess.Write))
+                {
+                    memoryStream.WriteTo(fs);
+                }
+
+                var fullPath = $"RoomImages/{fileName}";
+                return fullPath;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
