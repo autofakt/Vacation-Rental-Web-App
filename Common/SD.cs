91a6317861acad9dfa31fd36cd28a25d8ae51d11
Mike Be <autofakt5@gmail.com>
2021-11-12 16:57:25 -0800

    OrderRoomDetails Create method done in repo, 159.

--- Common/SD.cs
+++ Common/SD.cs
@@ -13,5 +13,13 @@ namespace Common
         public const string Role_Employee = "Employee";
 
         public const string Local_InitialBoking = "InitialRoomBookingInfo";
+
+        //Status codes for RoomOrderDetails
+        public const string Status_Pending = "Pending";
+        public const string Status_Booked = "Booked";
+        public const string Status_CheckedIn = "Checked-In";
+        public const string Status_CheckedOut = "Checked-Out";
+        public const string Status_NoShow = "NoShow";
+        public const string Status_Cancelled = "Cancelled";
     }
 }
