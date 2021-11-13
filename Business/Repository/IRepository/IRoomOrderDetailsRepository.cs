f17df505158c358c55ea3daf4ac3e490b18c09f1
Mike Be <autofakt5@gmail.com>
2021-11-12 16:24:11 -0800

    Added RoomOrderDetails Repository, 157.

---
+++ Business/Repository/IRepository/IRoomOrderDetailsRepository.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Repository.IRepository
+{
+    public interface IRoomOrderDetailsRepository
+    {
+        public Task<RoomOrderDetailsDTO> Create(RoomOrderDetailsDTO details);
+        public Task<RoomOrderDetailsDTO> MarkPaymentAsSuccessful(int id);
+        public Task<RoomOrderDetailsDTO> GetOrderRoomDetail(int roomOrderId);
+        public Task<IEnumerable<RoomOrderDetailsDTO>> GetAllOrderRoomDetails();
+        public Task<bool> UpdateOrderStatus(int roomOrderId, string status);
+        public Task<bool> IsRoomBooked(int RoomId, DateTime checkInDate, DateTime checkOutDate);
+    }
+}
