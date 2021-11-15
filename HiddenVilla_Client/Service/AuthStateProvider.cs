b30292792dbb31d5e4de19a32a51593b3f52e90e
Mike Be <autofakt5@gmail.com>
2021-11-15 12:30:29 -0800

    Working on jwt token, 187.

---
+++ HiddenVilla_Client/Service/AuthStateProvider.cs
@@ -0,0 +1,27 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization; //added this package
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Service
+{
+    public class AuthStateProvider: AuthenticationStateProvider
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorage;
+
+        public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _localStorage = localStorage;
+        }
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _localStorage.GetItemAsync<string>()
+        }
+    }
+}
