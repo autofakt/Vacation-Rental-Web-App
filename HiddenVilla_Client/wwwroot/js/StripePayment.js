4672b4fea029ae36334a48e1991aa974cda8715d
Mike Be <autofakt5@gmail.com>
2021-12-02 17:18:22 -0800

    finishing stripe , 176.

---
+++ HiddenVilla_Client/wwwroot/js/StripePayment.js
@@ -0,0 +1,6 @@
+﻿redirectToCheckout = function (sessionId) {
+    var stripe = Stripe("pk_test_51K2MBJCYtJ08h9bDIu6DghVg6J2qM0eEmSxuNxUhnhfC4cS8LZu0Fq009Cdxjz44gb8ADn0c7t39eYC79DLnZNdi00avMMtIsI");
+    stripe.redirectToCheckout({
+        sessionId: sessionId
+    });
+}
