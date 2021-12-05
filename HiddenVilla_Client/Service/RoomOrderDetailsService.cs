3b3a0a3977a592d559fcb1aa44bd366d17b70b4c
Mike Be <autofakt5@gmail.com>
2021-12-05 15:01:49 -0800

    Getting picture slides working in home and details pages.

--- HiddenVilla_Client/Service/RoomOrderDetailsService.cs
+++ HiddenVilla_Client/Service/RoomOrderDetailsService.cs
@@ -40,7 +40,7 @@ namespace HiddenVilla_Client.Service
 
         public async Task<RoomOrderDetailsDTO> SaveRoomOrderDetails(RoomOrderDetailsDTO details)
         {
-            details.UserId = "dummy user"; // need to remove later
+            //details.UserId = "dummy user"; // need to remove later
             var content = JsonConvert.SerializeObject(details);
             var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
             var response = await _client.PostAsync("api/roomorder/create", bodyContent);
