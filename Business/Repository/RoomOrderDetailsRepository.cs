5a84269ffdb6ef8067db87e48855cca26941462b
Mike Be <autofakt5@gmail.com>
2021-12-04 16:53:34 -0800

    done with stripe.

--- Business/Repository/RoomOrderDetailsRepository.cs
+++ Business/Repository/RoomOrderDetailsRepository.cs
@@ -73,42 +73,7 @@ namespace Business.Repository
             }
         }
 
-        public async Task<bool> IsRoomBooked(int RoomId, DateTime checkInDate, DateTime checkOutDate)
-        {
-            //confusing
-            var status = false;
-            var existingBooking = await _db.RoomOrderDetails.Where(x => x.RoomId == RoomId && x.IsPaymentSuccessful &&
-            //check if checkin date that user wants does not fall in between any dates for room that is booked
-            (checkInDate < x.CheckOutDate && checkInDate.Date > x.CheckInDate
-            //check if checkout date that user wnats does not fall in between any dates for rom that is booked
-            || checkOutDate.Date > x.CheckInDate.Date && checkInDate.Date < x.CheckInDate.Date
-            )).FirstOrDefaultAsync();
-
-            if(existingBooking != null)
-            {
-                status = true;
-            }
-            return status;
-
-            //RoomOrderDetails roomOrder = await _db.RoomOrderDetails.FirstOrDefaultAsync(x => x.RoomId == RoomId);
-            //if (roomOrder != null)
-            //{
-            //    var resultCheckIn = DateTime.Compare(checkInDate, roomOrder.CheckInDate);
-            //    var resultCheckOut = DateTime.Compare(checkOutDate, roomOrder.CheckOutDate);
-            //    //earlier than
-            //    if (resultCheckIn < 0 && resultCheckOut < 0)
-            //        return true;
-            //    //later than
-            //    else if (resultCheckIn > 0 && resultCheckOut > 0)
-            //        return true;
-            //    else
-            //        return false;
-            //}
-            //else
-            //{
-            //    return false;
-            //}
-        }
+        
 
         public async Task<RoomOrderDetailsDTO> MarkPaymentAsSuccessful(int id)
         {
